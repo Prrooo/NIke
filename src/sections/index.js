840b4f8b4a11a8481ec44ac592f6dfa849d9e6a1
Prrooo <harshmalik00011@gmail.com>
2023-10-21 10:45:57 +0530

    First Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import Services from './Services';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import CustomerReviews from './CustomerReviews';
+import SpecialOffer from './SpecialOffer';
+
+export{
+    Hero,
+    PopularProducts,
+    Footer,
+    Services,
+    SuperQuality,
+    Subscribe,
+    CustomerReviews,
+    SpecialOffer
+}
